106c7239f381b0908ee3f96573da411c6ca09417
cesarorrr <cesaro777rr@gmail.com>
2024-10-29 19:08:34 +0100

    DNI Input Deleted

--- UI_Streamlit.py
+++ UI_Streamlit.py
@@ -4,24 +4,19 @@ import streamlit as st
 st.title("Formulario de Registro")
 
 # Campos del formulario
-dni = st.text_input("DNI Cliente:")
-estacion = st.selectbox("Estación del año:", ["", "Invierno", "Verano"])
-numero_personas = st.number_input("Número de personas en la vivienda:", min_value=1, step=1)
-tipo_energia = st.selectbox("Tipo de energía:", ["", "Solar","Eólica","Hidroeléctrica","Nuclear","Carbón","Gas natural"])
+station = st.selectbox("Estación del año:", ["", "Invierno", "Verano"])
+nºperson = st.number_input("Número de personas en la vivienda:", min_value=1, step=1)
+energy_Type = st.selectbox("Tipo de energía:", ["", "Solar","Eólica","Hidroeléctrica","Nuclear","Carbón","Gas natural"])
 
 # Botón para enviar los datos
 if st.button("Enviar"):
     # Verificar si algún campo está vacío
-    if not dni or estacion == "" or tipo_energia == "":
+    if station == "" or energy_Type == "":
         st.error("Todos los campos deben ser completados.")
     else:
-        # Validar el patrón del DNI (8 dígitos y 1 letra)
-        if len(dni) == 9 and dni[:8].isdigit() and dni[-1].isalpha():
             # Mostrar los resultados
             st.success("Formulario enviado exitosamente!")
-            st.write("**DNI Cliente:**", dni)
-            st.write("**Estación del año:**", estacion)
-            st.write("**Número de personas en la vivienda:**", numero_personas)
-            st.write("**Tipo de energía:**", tipo_energia)
-        else:
-            st.error("El DNI debe tener 8 dígitos seguidos de 1 carácter (ej. 12345678A).")
+            st.write("**Estación del año:**", station)
+            st.write("**Número de personas en la vivienda:**", nºperson)
+            st.write("**Tipo de energía:**", energy_Type)
+ 
