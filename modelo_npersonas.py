2eee32485dc54ccecad3f054fae340820fc77edb
Joan <joanstucom@gmail.com>
2024-11-04 17:33:09 +0100

    minimal changes

--- modelo_npersonas.py
+++ modelo_npersonas.py
@@ -20,7 +20,7 @@ def calcular_personas(consumo: float, precio_energia: float, tipo_energia: str, 
     y = df['personas_vivienda']               # Variable objetivo
 
     # Dividir el conjunto de datos en entrenamiento y prueba (80%-20%)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=42)
 
     # Crear el modelo de regresión lineal
     model = LinearRegression()
